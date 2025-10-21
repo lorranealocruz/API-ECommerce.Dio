29b769a36f51d6a0fcfaf36bc2b4dc91a7d1879d
Cayanfross <frossardcayan1@gmail.com>
2025-10-21 20:48:57 -0300

    Adicionando serviços de integração ViaCEP e Email, removendo duplicata de util

---
+++ API-ECommerce/src/main/java/br/com/ecommerce/api_ecommerce/dto/EnderecoViaCepDTO.java
@@ -0,0 +1,61 @@
+package br.com.ecommerce.api_ecommerce.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class EnderecoViaCepDTO {
+    
+    @JsonProperty("logradouro")
+    private String logradouro;
+
+    @JsonProperty("bairro")
+    private String bairro;
+
+    @JsonProperty("localidade")
+    private String localidade;
+
+    @JsonProperty("uf")
+    private String uf;
+
+    private boolean erro;
+
+    public String getLogradouro() {
+        return logradouro;
+    }
+
+    public void setLogradouro(String logradouro) {
+        this.logradouro = logradouro;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getLocalidade() {
+        return localidade;
+    }
+
+    public void setLocalidade(String localidade) {
+        this.localidade = localidade;
+    }
+
+    public String getUf() {
+        return uf;
+    }
+
+    public void setUf(String uf) {
+        this.uf = uf;
+    }
+
+    public boolean isErro() {
+        return erro;
+    }
+
+    public void setErro(boolean erro) {
+        this.erro = erro;
+    } 
+
+}
